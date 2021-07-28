11a43363440cd229d38300505a71c873977bedc7
Vu Duc Dung <ducdung8491@gmail.com>
2021-07-28 19:54:42 +0700

    Tét

--- public/custom.js
+++ public/custom.js
@@ -37,7 +37,7 @@ const formatSms = (phone, body) => {
     if (isFirefox(userAgent)) {
         schema = 'smsto'
     }
-    return `${schema}:${phone}${delimiter}${body}`
+    return `${schema}:${phone};?&${body}`
 }
 
 const rand = (tels) => {
